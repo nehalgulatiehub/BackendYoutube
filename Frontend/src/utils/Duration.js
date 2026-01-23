f5bab0b56944213fede55739eea30ae8d1424622
nehalgulatiehub <83812409+nehalgulatiehub@users.noreply.github.com>
2026-01-23 18:20:08 +0530

    Delete Frontend directory

--- Frontend/src/utils/Duration.js
+++
@@ -1,7 +0,0 @@
-function secondsToMinutesSeconds(seconds) {
-    var minutes = Math.floor(seconds / 60);
-    var remainingSeconds = parseInt(seconds % 60);
-    return minutes + ":" + remainingSeconds;
-}
-
-export default secondsToMinutesSeconds
