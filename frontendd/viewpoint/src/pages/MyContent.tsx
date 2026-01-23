1936eddf168f5c1bfd342df6c46fe1700931c9e7
nehalgulatiehub <83812409+nehalgulatiehub@users.noreply.github.com>
2026-01-23 18:20:18 +0530

    Delete frontendd directory

--- frontendd/viewpoint/src/pages/MyContent.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import { useAuthStore } from '../store/autoStore'
-
-export const MyContent = () => {
-  const { user } = useAuthStore()
-  if (!user) return <Navigate to="/login" />
-  return <Navigate to={`/c/${user.username}`} />
-}
